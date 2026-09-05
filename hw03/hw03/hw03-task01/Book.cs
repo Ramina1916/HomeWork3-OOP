eef5b9b6454a2ed2203b8f73a0d470d52bfe36e3
Reihaneh Karimi <reihanehkarimi1174@gmail.com>
2026-09-05 19:54:29 +0330

    HomeWork03-Tasks

---
+++ hw03/hw03/hw03-task01/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hw03.hw03_task01
+{
+    public class Book
+    {
+        // class properties
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+        public bool IsAvailable { get; set; }
+
+        // constructor
+        public Book(string title, string author, string isbn)
+        {
+            Title = title;
+            Author = author;
+            ISBN = isbn;
+            IsAvailable = true; // by default, a new book is available 
+        }
+    }
+}
---
+++ hw03/hw03/hw03-task01/Library.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hw03.hw03_task01
+{
+    public class Library
+    {
+        // create a list of book
+        private List<Book> books = new List<Book>();
+
+        // add book to the library
+        public void AddBook(Book book)
+        {
+            // if user add null object
+            if (book == null)
+                throw new ArgumentNullException("Book cannot be null, friend!");
+            else
+            {
+                books.Add(book);
+                Console.WriteLine($"Book '{book.Title}' added successfully.");
+            }
+
+        }
+
+        // borrow a book from the library
+        public void BorrowBook(string title)
+        {
+            if (title == null)
+            {
+                throw new ArgumentNullException("Title cannot be null, friend!");
+            }
+            else if (books == null || books.Count == 0)
+            {
+                Console.WriteLine("No books available in the library :(");
+            }
+            else
+            {
+
+                foreach (Book book in books)
+                {
+                    if (book.Title == title)
+                    {
+                        if (book.IsAvailable)
+                        {
+                            book.IsAvailable = false;
+                            Console.WriteLine($"You borrowed '{book.Title}', have a good read :)");
+                            return; // stop the method
+                        }
+                        else
+                        {
+                            Console.WriteLine("Sorry! this book is not available right now :(");
+                            return;
+                        }
+                    }
+                }
+                Console.WriteLine("Book not found in the library :(");
+            }
+        }
+
+        // return a book to the library
+        public void ReturnBook(string title)
+        {
+            if (title == null)
+            {
+                throw new ArgumentNullException("Title cannot be null, friend!");
+            }
+            else
+            {
+                foreach (Book book in books)
+                {
+                    if (book.Title == title)
+                    {
+                        if (!book.IsAvailable)
+                        {
+                            book.IsAvailable = true;
+                            Console.WriteLine($"You returned '{book.Title}'.");
+                            return;
+                        }
+                        else
+                        {
+                            Console.WriteLine("This book is already available.");
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
