4f92edef66e6f830ae0adbdd86d2ad2ca900392a
Reihaneh Karimi <reihanehkarimi1174@gmail.com>
2026-09-05 23:04:49 +0330

    hw03-task03

---
+++ hw03/hw03-task03/hw03-task03/Electronic.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hw03_task03
+{
+    public class Electronic : Product, IDiscountable
+    {
+        // property
+        public int WarrantyPeriod { get; set; }
+
+        // constructor
+        public Electronic(string name, decimal price, int warranty) : base(name, price)
+        {
+            // validate warranty argument
+            if (warranty < 0) throw new ArgumentOutOfRangeException("Warranty Period can not be negative, buddy!");
+            WarrantyPeriod = warranty;
+        }
+
+        // detail method that overrides the base class method to include warranty information
+        public override string GetProductDetails() => $"{base.GetProductDetails()}, Warranty: {WarrantyPeriod} months";
+        
+
+        // Apply discount by mutating Price and return the new price
+        public decimal ApplyDiscount(decimal percent)
+        {
+            // validate percent argument
+            if (percent < 0 || percent > 100) throw new ArgumentOutOfRangeException("Percent must be between 0 and 100, friend!");
+            // calculate the new price after applying the discount and round to 2 decimal places
+            return Math.Round(Price * (1 - percent / 100m), 2); // m -> convert to decimal
+        }
+    }
+}
---
+++ hw03/hw03-task03/hw03-task03/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hw03_task03
+{
+    public class Product
+    {
+        // properties for product name and price
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        // constructor for product class that takes name and price as parameters
+        public Product(string name, decimal price)
+        {
+            if (name == null) throw new ArgumentNullException("Product name can not be null, buddy!");
+            if (price < 0) throw new ArgumentOutOfRangeException("Price cannot be negative, buddy!");
+            else
+            {
+                Name = name;
+                Price = price;
+            }
+        }
+
+        // a virtual method to get product details, which can be overridden in derived classes
+        public virtual string GetProductDetails() => $"Name: {Name}, Price: {Price:C}";
+
+    }
+}
